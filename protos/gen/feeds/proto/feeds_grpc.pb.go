1a55cdc7225d7caba522359f5b9ce8c79da23242
Nikita Ivanov <nikitatibirkin259@gmail.com>
2025-03-16 16:21:20 +0300

    init

--- protos/gen/feeds/proto/feeds_grpc.pb.go
+++ protos/gen/feeds/proto/feeds_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v5.29.3
 // source: proto/feeds.proto
 
-package feeds
+package feedsv1
 
 import (
 	context "context"
--- protos/gen/login/proto/login_grpc.pb.go
+++ protos/gen/login/proto/login_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v5.29.3
 // source: proto/login.proto
 
-package login
+package loginv1
 
 import (
 	context "context"
